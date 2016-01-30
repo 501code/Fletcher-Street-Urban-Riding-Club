9fa9dfa617e4f153faee0386d231613d2f537950
Nick Hargreaves <hargreaves1234@gmail.com>
2016-01-30 09:48:40 +0300

    init pages model + migration

--- pages/models.py
+++ pages/models.py
@@ -2,4 +2,8 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Page(models.Model):
+    title = models.CharField('Page Title', max_length=100)
+    body = models.CharField('Body', max_length=5000)
+    pub_date = models.DateField('Date Published',)
