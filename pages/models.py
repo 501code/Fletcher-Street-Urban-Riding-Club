47bfec9b210aaf404b55b5104c5de9b490673c94
Nick Hargreaves <hargreaves1234@gmail.com>
2016-01-30 10:01:46 +0300

    remove unnecessary files + update pages migration for body text

--- pages/models.py
+++ pages/models.py
@@ -5,5 +5,5 @@ from django.db import models
 
 class Page(models.Model):
     title = models.CharField('Page Title', max_length=100)
-    body = models.CharField('Body', max_length=5000)
+    body = models.TextField('Body', max_length=5000)
     pub_date = models.DateField('Date Published',)
