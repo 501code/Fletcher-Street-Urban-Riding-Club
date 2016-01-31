6de89ff29fce7664031b75b533bd31bf8388a59e
Nick Hargreaves <hargreaves1234@gmail.com>
2016-01-31 10:22:37 +0300

    disable delete action for site details

--- site_details/admin.py
+++ site_details/admin.py
@@ -8,4 +8,17 @@ class SiteDetailAdmin(admin.ModelAdmin):
     list_display = ['label', 'value']
     readonly_fields = ['label']
 
+    def has_add_permission(self, request):
+        return False
+
+    def get_actions(self, request):
+        #Disable delete
+        actions = super(SiteDetailAdmin, self).get_actions(request)
+        del actions['delete_selected']
+        return actions
+
+    def has_delete_permission(self, request, obj=None):
+        #Disable delete
+        return False
+
 admin.site.register(SiteDetail, SiteDetailAdmin)
