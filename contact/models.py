5b45990ecf189bc6543adeec323b67c12ea8b3c7
Nick Hargreaves <hargreaves1234@gmail.com>
2016-02-08 22:18:14 +0300

    contact model, register in admin

--- contact/models.py
+++ contact/models.py
@@ -2,4 +2,13 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    subject = models.CharField('Subject', max_length=100)
+    name = models.CharField('Full Names', max_length=100)
+    email = models.EmailField('Email')
+    body = models.TextField('Body', max_length=5000)
+    cc_myself = models.BooleanField('CC Sender', default=False)
+
+    def __str__(self):
+        return self.subject
--- fsurc/settings.py
+++ fsurc/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pages',
-    'site_details'
+    'site_details',
+    'contact'
 ]
 
 MIDDLEWARE_CLASSES = [
