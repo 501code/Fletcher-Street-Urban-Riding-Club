251b2a860df915d7e2ee8d3cdc63f13b6f0595b3
Nick Hargreaves <hargreaves1234@gmail.com>
2016-02-08 22:30:49 +0300

    make contact messages read only

--- contact/admin.py
+++ contact/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
 from .models import Contact
 
-# Register your models here.
-admin.site.register(Contact)
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ['subject', 'email']
+    readonly_fields = Contact._meta.get_all_field_names()
+
+    def has_add_permission(self, request):
+        return False
+
+    def get_actions(self, request):
+        # Disable delete
+        actions = super(ContactAdmin, self).get_actions(request)
+        del actions['delete_selected']
+        return actions
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+admin.site.register(Contact, ContactAdmin)
